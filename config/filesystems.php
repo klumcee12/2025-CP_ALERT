f2235b249001d57d1bdbfef19d2bfcda5ac712a8
klumcee12 <solis2003asiul@gmail.com>
2025-12-05 18:29:46 +0800

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,11 +40,11 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => '/home/u266115628/alertplus/storage/app/public',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
+        ],
+
         ],
 
         's3' => [
